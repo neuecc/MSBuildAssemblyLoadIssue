919dd74dc18a51d63827ce234339ff1b1c15322d
neuecc <ils@neue.cc>
2020-01-07 17:00:54 +0900

    code

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using MagicOnion;
+using MessagePack;
+
+namespace MSBuildAssemblyLoadIssue
+{
+    [MessagePackObject]
+    public class Foo
+    {
+        [Key(0)]
+        public int Bar { get; set; }
+    }
+
+    public interface IFooService : IService<IFooService>
+    {
+        UnaryResult<int> BarAsync(Foo foo);
+    }
+}
